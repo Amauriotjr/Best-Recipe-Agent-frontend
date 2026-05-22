0bb20acf7877947bf091fc345931210057be676b
Amauriotjr <amauri.junior@sergipetec.org.br>
2026-05-22 17:50:47 +0300

    update api

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+const API_URL = import.meta.env.VITE_API_URL || "https://best-recipe-agent-backend.onrender.com";
 
 export async function getRecipeRecommendations(ingredients) {
   const response = await fetch(`${API_URL}/recommend`, {
